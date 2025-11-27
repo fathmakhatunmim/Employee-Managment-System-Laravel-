159a014bdecd207e3b514d8d4c76748e79e8ae66
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-11-27 12:20:21 +0600

    create design

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -3,11 +3,70 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Employee Managment system</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+   <div class="bg-primary py-3">
+    <h1 class="text-white text-center">Employee Management System</h1>
+   </div>
+
+
+
+      <div class='container'>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10">
+                <div class="card border-0 shadow-lg my-5">
+                    <div class="card-header">
+                        <h3>CREATE</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Name</label>
+                            <input type="text" class="form-control form-control-lg" 
+                            name="name" id="" placeholder="name">
+
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="" class="form-label">Email</label>
+                            <input type="text" class="form-control form-control-lg" 
+                            name="name" id="" placeholder="Email">
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Position</label>
+                            <input type="email" class="form-control form-control-lg" 
+                            name="name" id="" placeholder="position">
+
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">department</label>
+                            <input type="text" class="form-control form-control-lg" 
+                            name="name" id="" placeholder="department">
+
+                        </div>
+
+                         <div class="d-grid">
+                            <button class="btn btn-lg btn-primary">
+                               SUBMIT
+                            </button>
+                         </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+      </div>
+    
+
+
+
   </body>
 </html>
